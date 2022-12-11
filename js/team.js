737a2ecb3fd78a59751cc89c81ff525ee9148df3
yywbadm <100027673+yywbadm@users.noreply.github.com>
2022-12-11 14:54:14 +0800

    Update team.js

--- js/team.js
+++ js/team.js
@@ -33,31 +33,3 @@ for (let indexOne = 0; indexOne < owners.length; indexOne++) {
             boxOwners.innerHTML += ownerList;
         });
 }
-
-const API = "https://discord-web-api.glitch.me/discord/user/";
-
-const owners = [
-    {
-        "id": "848164182334898216",
-        "post": "開發者",
-        "GHURL": "https://github.com/Tira-tw",
-        "InstaURL": "",
-        "YTURL": "https://youtube.com/c/tiratw"
-    }
-]
-
-for (let indexOne = 0; indexOne < owners.length; indexOne++) {
-    const elementOwners = owners[indexOne];
-
-    $.getJSON(API + elementOwners.id)
-        .then(output => {
-            if (!output.username || !output.url) {
-                setTimeout(function () {
-                    document.querySelectorAll(".banner img").forEach(imgs => imgs.src = url + "../assets/bot.png");
-                }, 1000);
-            }
-
-            const ownerList = "<div id='trigger' class='card' style='margin: 15px;'><div class='banner'><img src='" + output.url + "'></div></br></br></br></br><h2 class='name'>" + output.username + "</h2><div class='title'><h1 id='trigger2' style='font-size: 26px; color: #000000;'>" + elementOwners.post + "</h2></div><div class='actions'><div class='follow-btn' style='margin-bottom: 5px;'><a href='" + elementOwners.GHURL + "' target='_blank'><button style='color: #000000;'>GitHub</button></a></div><div class='follow-btn'><a href='" + elementOwners.YTURL + "' target='_blank'><button style='color: #000000;'>YouTube</button></a></div></div></div>"
-            boxOwners.innerHTML += ownerList;
-        });
-}
